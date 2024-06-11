48b7015f44eea9142d43020f342c0ad590ba0605
ITuzov <ITuzov@korusconsulting.ru>
2024-06-12 02:03:38 +0300

    add frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { TransformInterceptor } from './transform.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Укажите здесь ваш клиентский домен
+    origin: 'https://ituzov.fun', // Укажите здесь ваш клиентский домен
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
